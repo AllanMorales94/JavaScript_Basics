74b1c46abab21f96a36f210cf9feb1f2e03b81f2
Allan Morales <overlydedicated1994@gmail.com>
2023-01-06 19:50:41 -0800

    uploading extra file extra folder for extra javascript fun

---
+++ assets/js/script2.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    alert("Welcome to Functional JavaScript!");
+  };
+
+  function alertHello(){
+    alert("Success is Disguised as Failure!")
+}
+
+let firstName = prompt("Hello there!", " Enter name here to start.." );
+  /* Collect Input 
+  prompt(text, defaultText); */
+
+  /* Display Output 
+   alert("Hi" + firstName + "!!!");
+   document.write("hi"); used to display text on page */
